a87929cb473f41c8835d5cd4dddc2fbdf13c1e6d
himono <shin.h.20040720@gmail.com>
2023-09-13 15:24:33 +0900

    設定画面でデバイス制御に送るHIDをドライバから取得できる形に整形

--- GalaFli/SettingForm.cs
+++ GalaFli/SettingForm.cs
@@ -9,7 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Management;
 using System.Runtime.InteropServices;
-
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace GalaFli
 {
@@ -34,7 +35,7 @@ namespace GalaFli
         private void Form1_Load(object sender, EventArgs e)
         {
             // USBデバイス情報を取得するためのクエリを作成
-            string query = "SELECT * FROM Win32_Keyboard WHERE ConfigManagerErrorCode = 0 AND DeviceID Like 'HID%'";
+            string query = "SELECT * FROM Win32_KeyBoard WHERE ConfigManagerErrorCode = 0";
 
             ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
             ManagementObjectCollection usbDevices = searcher.Get();
@@ -57,27 +58,85 @@ namespace GalaFli
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+
+
             // 選択されたUSBデバイスの情報を表示
             KeyValuePair selectedDevice = (KeyValuePair)comboBox1.SelectedItem;
             MessageBox.Show($"デバイス名: {selectedDevice.Key}\nデバイスID: {selectedDevice.Value}");
+            
+            
+
+
         }
 
         //保存ボタンの処理
         private void buttonSave_Click(object sender, EventArgs e)
         {
+            //デバイスが選択されていなかったらエラー
             if(comboBox1.SelectedItem == null)
             {
                 MessageBox.Show("デバイスを選択してください", "エラー");
                 return;
             }
             KeyValuePair saveDevice = (KeyValuePair)comboBox1.SelectedItem;
-            
+
             bool[] ret = new bool[5];
-            ret[0] = WritePrivateProfileString("Tenkey", "deviceId", saveDevice.Value, ".\\TenkeySettings.ini");
+            //Interceprtionで取得しているHIDの形式に変換して保存する
+            //Win32_Keyboardで取得した"USB\VID_PID"の形式を"HID\VID_PID"に変換してWin32_PnPEntityに投げ、欲しいHIDを取得する
+            if (Regex.IsMatch(saveDevice.Value, ".+VID.+PID.+"))
+            {
+                string HIDsearchperser = saveDevice.Value.ToString();
+                string searchTerm = "PID"; // 置換対象の文字列
+                int startIndex = HIDsearchperser.IndexOf(searchTerm);
+                if (startIndex >= 0)
+                {
+                    int endIndex = startIndex + searchTerm.Length + 5; // "PID_" + 5文字以降のインデックス
+                    string replacement = HIDsearchperser.Substring(0, endIndex);
+                    HIDsearchperser = replacement;
+                }
+                else
+                {
+                    Console.WriteLine("置換対象が見つかりませんでした。");
+                }
+
+
+                HIDsearchperser = HIDsearchperser.Replace("USB", "HID");
+                HIDsearchperser = HIDsearchperser.Replace("\\", "\\\\");
+                Debug.WriteLine(HIDsearchperser);
+                string query = "SELECT * FROM Win32_PnPEntity WHERE ConfigManagerErrorCode = 0 AND DeviceID LIKE '%" + HIDsearchperser + "%'";
+                Debug.WriteLine(query);
+                ManagementObjectSearcher sercher = new ManagementObjectSearcher(query);
+                ManagementObjectCollection selectedDevices = sercher.Get();
+                foreach (ManagementObject obj in selectedDevices)
+                {
+                    // ハードウェアIDを取得
+                    string[] hardwareIDs = (string[])obj["HardwareID"];
+                    if (hardwareIDs != null)
+                    {
+                        Debug.WriteLine("Hardware IDs:");
+                        foreach (string hardwareID in hardwareIDs)
+                        {
+                            if (Regex.IsMatch(hardwareID, "HID.+VID.+PID.+REV")) {
+                                ret[0] = WritePrivateProfileString("Tenkey", "deviceId", hardwareID, ".\\TenkeySettings.ini");
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("このデバイスは使用できません、違うデバイスを選択して下さい", "エラー");
+                return;
+            }
+            
+
+
+
             ret[1] = WritePrivateProfileString("Tenkey", "isTab", isTabNumlock.Checked.ToString(), ".\\TenkeySettings.ini");
             ret[2] = WritePrivateProfileString("Tenkey", "isBSUpper", isBackSpace.Checked.ToString(), ".\\TenkeySettings.ini");
             ret[3] = WritePrivateProfileString("Tenkey", "isZeroUnion", isIntegration.Checked.ToString(), ".\\TenkeySettings.ini");
             ret[4] = WritePrivateProfileString("Tenkey", "isZeroThree", isThreeZeros.Checked.ToString(), ".\\TenkeySettings.ini");
+
 
 
             Application.Restart();
@@ -87,7 +146,7 @@ namespace GalaFli
         {
             comboBox1.Items.Clear();
             // USBデバイス情報を取得するためのクエリを作成
-            string query = "SELECT * FROM Win32_Keyboard WHERE ConfigManagerErrorCode = 0 AND DeviceID Like 'HID%'";
+            string query = "SELECT * FROM Win32_USBHub WHERE ConfigManagerErrorCode = 0";
 
             ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
             ManagementObjectCollection usbDevices = searcher.Get();
